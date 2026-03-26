907ff31966ac948060edb5869d62c7dff273d057
Matias Lopez <matutelopez@hotmail.com>
2026-03-26 18:23:22 -0300

    [main] Repository: add async

--- namasdev.Data.en/IReadOnlyRepository.cs
+++ namasdev.Data.en/IReadOnlyRepository.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
 
 using namasdev.Core.Entity;
 using namasdev.Core.Linq;
@@ -12,13 +14,22 @@ namespace namasdev.Data
         where TId : IEquatable<TId>
     {
         TEntity Get(TId id, bool includeDeleted = false);
+        Task<TEntity> GetAsync(TId id, bool includeDeleted = false, CancellationToken ct = default);
         TEntity Get(TId id, IEnumerable<string> loadProperties, bool includeDeleted = false);
+        Task<TEntity> GetAsync(TId id, IEnumerable<string> loadProperties, bool includeDeleted = false, CancellationToken ct = default);
         TEntity Get(TId id, IEnumerable<Expression<Func<TEntity, object>>> loadProperties, bool includeDeleted = false);
+        Task<TEntity> GetAsync(TId id, IEnumerable<Expression<Func<TEntity, object>>> loadProperties, bool includeDeleted = false, CancellationToken ct = default);
         TEntity Get(TId id, ILoadProperties<TEntity> loadProperties, bool includeDeleted = false);
+        Task<TEntity> GetAsync(TId id, ILoadProperties<TEntity> loadProperties, bool includeDeleted = false, CancellationToken ct = default);
         IEnumerable<TEntity> GetList(bool includeDeleted = false, OrderAndPagingParameters op = null);
+        Task<IEnumerable<TEntity>> GetListAsync(bool includeDeleted = false, OrderAndPagingParameters op = null, CancellationToken ct = default);
         IEnumerable<TEntity> GetList(IEnumerable<string> loadProperties, bool includeDeleted = false, OrderAndPagingParameters op = null);
+        Task<IEnumerable<TEntity>> GetListAsync(IEnumerable<string> loadProperties, bool includeDeleted = false, OrderAndPagingParameters op = null, CancellationToken ct = default);
         IEnumerable<TEntity> GetList(IEnumerable<Expression<Func<TEntity, object>>> loadProperties, bool includeDeleted = false, OrderAndPagingParameters op = null);
+        Task<IEnumerable<TEntity>> GetListAsync(IEnumerable<Expression<Func<TEntity, object>>> loadProperties, bool includeDeleted = false, OrderAndPagingParameters op = null, CancellationToken ct = default);
         IEnumerable<TEntity> GetList(ILoadProperties<TEntity> loadProperties, bool includeDeleted = false, OrderAndPagingParameters op = null);
+        Task<IEnumerable<TEntity>> GetListAsync(ILoadProperties<TEntity> loadProperties, bool includeDeleted = false, OrderAndPagingParameters op = null, CancellationToken ct = default);
         bool ExistsById(TId id, bool includeDeleted = false);
+        Task<bool> ExistsByIdAsync(TId id, bool includeDeleted = false, CancellationToken ct = default);
     }
 }
--- namasdev.Data.en/IRepository.cs
+++ namasdev.Data.en/IRepository.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 using namasdev.Core.Entity;
 
@@ -10,16 +12,28 @@ namespace namasdev.Data
         where TId : IEquatable<TId>
     {
         void Add(IEnumerable<TEntity> entities, int batchSize = 100);
+        Task AddAsync(IEnumerable<TEntity> entities, int batchSize = 100, CancellationToken ct = default);
         void Add(TEntity entity);
+        Task AddAsync(TEntity entity, CancellationToken ct = default);
         void Update(IEnumerable<TEntity> entities, int batchSize = 100);
+        Task UpdateAsync(IEnumerable<TEntity> entities, int batchSize = 100, CancellationToken ct = default);
         void Update(TEntity entity);
+        Task UpdateAsync(TEntity entity, CancellationToken ct = default);
         void UpdateProperties(IEnumerable<TEntity> entities, int batchSize = 100, params string[] properties);
+        Task UpdatePropertiesAsync(IEnumerable<TEntity> entities, int batchSize = 100, string[] properties = null, CancellationToken ct = default);
         void UpdateProperties(TEntity entity, params string[] properties);
+        Task UpdatePropertiesAsync(TEntity entity, string[] properties = null, CancellationToken ct = default);
         void UpdateDeletedProperties(TEntity entity);
+        Task UpdateDeletedPropertiesAsync(TEntity entity, CancellationToken ct = default);
         void UpdateDeletedProperties(IEnumerable<TEntity> entities, int batchSize = 100);
+        Task UpdateDeletedPropertiesAsync(IEnumerable<TEntity> entities, int batchSize = 100, CancellationToken ct = default);
         void Delete(IEnumerable<TEntity> entities, int batchSize = 100);
+        Task DeleteAsync(IEnumerable<TEntity> entities, int batchSize = 100, CancellationToken ct = default);
         void Delete(TEntity entity);
+        Task DeleteAsync(TEntity entity, CancellationToken ct = default);
         void DeleteByIds(IEnumerable<TId> ids, int batchSize = 100);
+        Task DeleteByIdsAsync(IEnumerable<TId> ids, int batchSize = 100, CancellationToken ct = default);
         void DeleteById(TId id);
+        Task DeleteByIdAsync(TId id, CancellationToken ct = default);
     }
 }
